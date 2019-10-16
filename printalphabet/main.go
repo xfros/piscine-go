8e41b74f214768c1b0b955178e18722476f1cb53
xfros <i.fros95@gmail.com>
2019-10-16 21:48:25 +0600

    go

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,18 +1,14 @@
 package main
 
 import "github.com/01-edu/z01"
-//import "fmt"
 
 func main() {
 
+		for i:= 0; i <= 25; i++ {
 	
-
-	for i:= 0; i <= 25; i++ {
-
-		//fmt.Println(i)
-		
 		var aRune string = "abcdefghijklmnopqrstuvwxyz"
 
 		z01.PrintRune(rune(aRune[i]))
-	}
+    }
+    
 }
