ddb1e9d0e2d4051a699e097deafbd230c83dcb78
xfros <i.fros95@gmail.com>
2019-10-16 22:03:01 +0600

    dfs

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,11 +3,9 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-
-		for i:= 0; i <= 25; i++ {
-	
-				var aRune string = "abcdefghijklmnopqrstuvwxyz"
-
-				z01.PrintRune(rune(aRune[i]))
+	for i:= 0; i <= 25; i++ {
+		var aRune string = "abcdefghijklmnopqrstuvwxyz"
+		z01.PrintRune(rune(aRune[i]))
     }
    }
+   
