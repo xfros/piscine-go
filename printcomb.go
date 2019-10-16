6b9e69c548a75ccdae8adc6aa89d8efd609a04b6
xfros <i.fros95@gmail.com>
2019-10-16 23:51:10 +0600

    sdf

--- printcomb.go
+++ printcomb.go
@@ -9,9 +9,8 @@ func PrintComb() {
 				z01.PrintRune(a)
 				z01.PrintRune(b)
 				z01.PrintRune(c)
-
-					z01.PrintRune(44)
-					z01.PrintRune(32)
+				z01.PrintRune(44)
+				z01.PrintRune(32)
 
 			}
 		}
