8e43b1680e25f199a7a49f242381f4eff0b8b894
xfros <i.fros95@gmail.com>
2019-10-16 23:27:25 +0600

    sdf

--- isnegative.go
+++ isnegative.go
@@ -1,11 +1,11 @@
 package piscine
-import  "github.com/01-edu/z01"
-func IsNegative (nb int){
+import "github.com/01-edu/z01"
+func IsNegative(nb int){
 	if nb > 0 {
 		z01.PrintRune('F')
-		} else if nb == 0 {
+	} else if nb == 0 {
 			z01.PrintRune('F')
-			} else {
-				z01.PrintRune('T')
-			}
+	} else {
+			z01.PrintRune('T')
 	}
+}
