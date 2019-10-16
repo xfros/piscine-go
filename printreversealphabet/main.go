23fa5fc3715cd13e2ab035bc0b27b75c1c9b18ac
xfros <i.fros95@gmail.com>
2019-10-16 22:26:43 +0600

    sf

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -5,8 +5,8 @@ import "github.com/01-edu/z01"
 func main() {
 	i := 25
 	for i >= 0 {
-		var aRune string = "abcdefghijklmnopqrstuvwxyz"
-		z01.PrintRune(rune(aRune[i]))
+		var xrune string = "abcdefghijklmnopqrstuvwxyz"
+		z01.PrintRune(rune(xrune[i]))
 		i--
 	}
 	z01.PrintRune(10)
