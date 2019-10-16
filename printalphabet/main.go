1cb03dd6b1382d64c312466f362e3c8cd950ff53
xfros <i.fros95@gmail.com>
2019-10-16 21:41:54 +0600

    go alphabet

---
+++ printalphabet/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/01-edu/z01"
+//import "fmt"
+
+func main() {
+
+	
+
+	for i:= 0; i <= 25; i++ {
+
+		//fmt.Println(i)
+		
+		var aRune string = "abcdefghijklmnopqrstuvwxyz"
+
+		z01.PrintRune(rune(aRune[i]))
+	}
+}
